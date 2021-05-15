b4f15deee5cd740151494727146dca640f5aa9db
Harvinder5 <harvinder.singh@crownstack.com>
2021-05-15 20:29:17 +0530

    android notification popup

--- rnarch/App.tsx
+++ rnarch/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import theme from './src/styles/theme';
 import { ThemeProvider } from '@shopify/restyle';
@@ -8,7 +8,10 @@ import LoadAssets from './src/utils/LoadAssets';
 import { Main } from './src/screens/Navigation';
 import { I18nProvider } from './src/locales/I18nProvider';
 import { AuthProvider } from './src/context/Authentication';
-export default from './storybook';
+import PushNotification, { Importance } from 'react-native-push-notification';
+import messaging from '@react-native-firebase/messaging';
+
+// export default from './storybook';
 
 const paperTheme: typeof DefaultTheme = {
   ...DefaultTheme,
@@ -28,7 +31,57 @@ const paperTheme: typeof DefaultTheme = {
 
 const Providers = composeComponents(SafeAreaProvider, AuthProvider);
 
+export const getNotificationToken = async () => {
+  await messaging().requestPermission();
+  console.log('jhgdhjs');
+  try {
+    await messaging().registerDeviceForRemoteMessages();
+    const fcmToken = await messaging().getToken();
+    console.log({ fcmToken });
+    return fcmToken;
+  } catch (err) {
+    console.log(err);
+  }
+  return undefined;
+};
+
+PushNotification.createChannel(
+  {
+    channelId: 'xyz', // same as defined in androidmanifest or you can make multiple channels
+    channelName: 'My channel', // (required)
+    channelDescription: 'A channel to categorise your notifications', // (optional) default: undefined.
+    playSound: true, // (optional) default: true
+    soundName: 'default', // (optional) See `soundName` parameter of `localNotification` function
+    importance: Importance.HIGH, // (optional) default: Importance.HIGH. Int value of the Android notification importance
+    vibrate: true, // (optional) default: true. Creates the default vibration patten if true.
+  },
+  created => console.log(`createChannel returned '${created}'`), // (optional) callback returns whether the channel was created, false means it already existed.
+);
+
 const App = () => {
+  useEffect(() => {
+    getNotificationToken();
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      // Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+      console.log({ remoteMessage });
+      PushNotification.localNotification({
+        /* Android Only Properties */
+        channelId: 'xyz',
+        title: remoteMessage.notification.title,
+        message: remoteMessage.notification.body,
+        data: {},
+        playSound: true, // (optional) default: true
+        soundName: 'default',
+        visibility: 'public', // (optional) set notification visibility, default: private
+        priority: 'high', // (optional) set notification priority, default: high
+        // bigPictureUrl: remoteMessage.notification.android.imageUrl, // (optional) default: "ic_launcher". Use "" for no large icon.
+        // largeIconUrl: remoteMessage.notification.android.imageUrl,
+      });
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <LoadAssets>
       <Providers>
